24391ba13f735ed32271facf483ec7b3c93b58ed
Adriano Costa <adrianojesuscosta@gmail.com>
2025-08-17 15:29:01 -0300

    terceiro commit

--- backend/webstore/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-from django.utils.text import slugify
-
-# Create your models here.
-class TimeStampModel(models.Model):
-    create_at = models.DateTimeField(auto_now_add=True) 
-    update_at = models.DateTimeField(auto_now=True)
